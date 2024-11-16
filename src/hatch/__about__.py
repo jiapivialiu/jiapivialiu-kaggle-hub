b4860e47e877f9ed6d422606c008072df16252b7
Jiaping(Olivia) Liu <jiaping.liu@stat.ubc.ca>
2024-11-16 13:41:03 -0800

    init push

---
+++ src/hatch/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Jiaping(Olivia) Liu <jiaping.liu@stat.ubc.ca>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/hatch/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Jiaping(Olivia) Liu <jiaping.liu@stat.ubc.ca>
+#
+# SPDX-License-Identifier: MIT
